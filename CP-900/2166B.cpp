e5b70f45f6088cf5b53cadd094aca44ece82544c
PRANAVsh07 <studyjee619@gmail.com>
2026-09-22 22:58:26 +0530

    solve 2166B

---
+++ CP-900/2166B.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+
+
+    while(t--){
+        double a,b,n;
+        cin>>a>>b>>n;
+     
+
+
+        if(n*b<=a||a==b){
+            cout<<"1"<<endl;
+        }
+        else{
+            cout<<"2"<<endl;
+        }
+}
+}
